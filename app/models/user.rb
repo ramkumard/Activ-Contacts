3261bde0a818db0904831f276ea6c9b26c9c61c0
ramkumar <ram.kumar@styletag.com>
2014-02-07 09:52:50 +0530

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,:omniauthable
+  attr_accessor :login
+  has_many :authentications
+  def self.find_first_by_auth_conditions(warden_conditions) 
+      conditions = warden_conditions.dup 
+      if login = conditions.delete(:login) 
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first 
+      end 
+  end 
+  
+    def self.new_with_session(params,session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"],without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def self.from_omniauth(auth, current_user)
+    authorization = Authentication.where(:provider => auth.provider, :uid => auth.uid.to_s).first_or_initialize
+    if authorization.user.blank?
+      user = current_user.nil? ? User.where('email = ?', auth["info"]["email"]).first : current_user
+      if user.blank?
+       user = User.new
+       user.password = Devise.friendly_token[0,10]
+       user.username = auth.info.nickname
+       user.email = auth.info.email
+       auth.provider == "twitter" ?  user.save(:validate => false) :  user.save
+     end
+     authorization.user_id = user.id
+     authorization.save
+   end
+   authorization.user
+ end
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contacts::Application.config.secret_key_base = '283df3a3d6f83e344f92497e16da3e2a65016a478d38e1f7ce88f1c3f35dadc167df96ac2f79960a8770d58d69354fe3759eca5f3dcff63aa6515cb4187a15de'
